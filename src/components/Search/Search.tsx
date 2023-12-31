03e8d996333dfc1f32223dcd5d404588901fa826
Puskar-Roy <puskarroy600@gmail.com>
2023-12-31 19:11:27 +0530

    feat : bg image

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,10 @@
 import './Search.css'
+import image from '../../assets/4.png'
 const Search = () => {
   return (
     <section className='searchSection'>
       <div className="bgContainer">
+        <img className='bgImage' src={image} alt="image" />
         <div className="searchInput">
             <input type="text" placeholder='Enter Your Domain Link...' />
             {/* <button className='btnSearch'> Search</button> */}
